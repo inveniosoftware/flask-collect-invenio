90177d10f90686d502d70e28c09e3bfcbff5f930
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 12:13:52 +0200

    chore(lint): reformat with black and fix linter complaints

--- docs/conf.py
+++ docs/conf.py
@@ -2,35 +2,44 @@
 # SPDX-FileCopyrightText: 2021 TU Wien.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import os
-import re
 import datetime
+import os
 
 # Get the version string.  Cannot be done with import!
 g = {}
-with open(os.path.join('..', 'flask_collect', 'version.py'), 'rt') as fp:
+with open(os.path.join("..", "flask_collect", "version.py"), "rt") as fp:
     exec(fp.read(), g)
     version = g["__version__"]
 
 # The full version, including alpha/beta/rc tags.
 release = version
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
-templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'index'
-project = u'Flask-Collect-Invenio'
-copyright = u'%s, Kirill Klenov' % datetime.datetime.now().year
-exclude_patterns = ['_build']
-htmlhelp_basename = 'Flask-Collect-Inveniodoc'
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+project = "Flask-Collect-Invenio"
+copyright = "%s, Kirill Klenov" % datetime.datetime.now().year
+exclude_patterns = ["_build"]
+htmlhelp_basename = "Flask-Collect-Inveniodoc"
 latex_documents = [
-    ('index', 'Flask-Collect-Invenio.tex', u'Flask-Collect-Invenio Documentation',
-        u'Kirill Klenov', 'manual'),
+    (
+        "index",
+        "Flask-Collect-Invenio.tex",
+        "Flask-Collect-Invenio Documentation",
+        "Kirill Klenov",
+        "manual",
+    ),
 ]
 latex_elements = {}
 man_pages = [
-    ('index', 'flask-mixer', u'Flask-Collect-Invenio Documentation',
-     [u'Kirill Klenov'], 1)
+    (
+        "index",
+        "flask-mixer",
+        "Flask-Collect-Invenio Documentation",
+        ["Kirill Klenov"],
+        1,
+    )
 ]
-pygments_style = 'sphinx'
-html_theme = 'alabaster'
+pygments_style = "sphinx"
+html_theme = "alabaster"
 html_theme_options = {}
